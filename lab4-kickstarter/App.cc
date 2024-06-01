f3f7bfbd4f10276a5cbb0df5cbf8bc560237331d
Manu <manu.saharrea@mi.unc.edu.ar>
2024-06-01 13:21:48 -0300

    Algoritmo de enrutamiento TERMINADOOOO

--- lab4-kickstarter/App.cc
+++ lab4-kickstarter/App.cc
@@ -68,6 +68,7 @@ void App::handleMessage(cMessage *msg) {
         pkt->setByteLength(par("packetByteSize"));
         pkt->setSource(this->getParentModule()->getIndex());
         pkt->setDestination(par("destination"));
+        pkt->setKind(0);
         // send to net layer
         send(pkt, "toNet$o");
 
--- lab4-kickstarter/Net.cc
+++ lab4-kickstarter/Net.cc
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <omnetpp.h>
 #include <packet_m.h>
+#include <iostream>
 
 using namespace omnetpp;
 
@@ -38,6 +39,7 @@ Net::Net() {
 Net::~Net() {
 }
 
+
 void Net::initialize() {
 
     bool BestWay = false;
@@ -45,20 +47,21 @@ void Net::initialize() {
     int hops_antihorario = 0;
     int hops_horario = 0;
 
-    Packet *pkt_horario = new packet();
+    Packet *pkt_horario = new Packet();
     pkt_horario->setSource(this->getParentModule()->getIndex());
     pkt_horario->setDestination(par("destination"));
     pkt_horario->setKind(2);
     pkt_horario->setHopCount(0);
+    pkt_horario->setByteLength(par("packetByteSize"));
     send(pkt_horario,"toLnk$o",0);
 
-    Packet *pkt_antihorario = new packet();
+    Packet *pkt_antihorario = new Packet();
     pkt_antihorario->setSource(this->getParentModule()->getIndex());
     pkt_antihorario->setDestination(par("destination"));
     pkt_antihorario->setKind(3);
     pkt_antihorario->setHopCount(0);
+    pkt_antihorario->setByteLength(par("packetByteSize"));
     send(pkt_antihorario,"toLnk$o",1);
-
 }
 
 void Net::finish() {
@@ -66,16 +69,17 @@ void Net::finish() {
 
 void Net::sendPacket(Packet *pkt){
 
-    if (hops_antihorario > hops_horario){
+    if (hops_antihorario < hops_horario){
         out = 0;
     }
-    else if (hops_antihorario < hops_horario){
+    else if (hops_antihorario > hops_horario){
         out = 1;
     }
     else {
         out = rand() % 2;
     }
-    send(pkt, "toLnk$0", out);
+
+    send(pkt, "toLnk$o", out);
 
 }
 
@@ -91,40 +95,70 @@ void Net::handleMessage(cMessage *msg) {
     // All msg (events) on net are packets
     Packet *pkt = (Packet *) msg;
 
+    // tipo 2 = explorador horario
+    // tipo 3 = explorador antihorario
+    // tipo 4 = vuelta horario
+    // tipo 5 = vuelta antihorario
     // If this node is the final destination, and packet is type 2 or 3
     if (pkt->getDestination() == this->getParentModule()->getIndex() && pkt->getKind() == 2){
-        hops_horario = pkt->getHopCount();
+        Packet *back = new Packet();
+        back->setSource(this->getParentModule()->getIndex());
+        back->setDestination(pkt->getSource());
+        back->setKind(4);
+        back->setHopCount(0);
+        back->setByteLength(par("packetByteSize"));
         delete (pkt);
+        pkt = nullptr;
+        send(back,"toLnk$o",0);
 
     } else if (pkt->getDestination() == this->getParentModule()->getIndex() && pkt->getKind() == 3){
-        hops_antihorario = pkt->getHopCount();
+        Packet *back = new Packet();
+        back->setSource(this->getParentModule()->getIndex());
+        back->setDestination(pkt->getSource());
+        back->setKind(5);
+        back->setHopCount(0);
+        back->setByteLength(par("packetByteSize"));
         delete (pkt);
-    }
+        pkt = nullptr;
+        send(back,"toLnk$o",1);
 
+    } else if(pkt->getDestination() == this->getParentModule()->getIndex() && pkt->getKind() == 4){
+        hops_horario = pkt->getHopCount();
+        delete(pkt);
+        pkt = nullptr;
+
+    } else if (pkt->getDestination() == this->getParentModule()->getIndex() && pkt->getKind() == 5){
+        hops_antihorario = pkt->getHopCount();
+        delete(pkt);
+        pkt = nullptr;
+
+    }
     if (hops_antihorario != 0 && hops_horario != 0){
         BestWay = true;
     }
-    // If this node is the final destination, send to App
-    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
-        send(msg, "toApp$o");
-    } else if (pkt->arrivedOn("toLnk$i")){
-    // If not, forward the packet to some else... to who?
-        if (pkt->getKind() == 2 or pkt->getKind() == 3){
-            pkt->setHopCount(pkt->getHopCount() + 1);
-        }
+    if (pkt != nullptr){
+        if (pkt->getDestination() == this->getParentModule()->getIndex()) {
+                send(msg, "toApp$o");
 
-        if (pkt->arrivedOn("toLnk$i",0)) {
-            send(pkt,"toLnk$o",1);
+        } else if (pkt->arrivedOn("toLnk$i")){
+
+        // If not, forward the packet to some else... to who?
+                if (pkt->getKind() == 2 or pkt->getKind() == 3 or pkt->getKind() == 4 or pkt->getKind() == 5){
+                    pkt->setHopCount(pkt->getHopCount() + 1);
+                }
+
+                if (pkt->arrivedOn("toLnk$i",0)) {
+                    send(pkt,"toLnk$o",1);
+                } else {
+                    send(pkt,"toLnk$o",0);
+                }
         } else {
-            send(pkt,"toLnk$o",0);
-        }
-    } else {
-        assert(pkt->arrivedOn("toApp$i"));
-
-        buffer.insert(pkt);
-
-        if(BestWay){
-            sendPacketBuff();
+                assert(pkt->arrivedOn("toApp$i"));
+                buffer.insert(pkt);
+                if(BestWay){
+                    sendPacketBuff();
+                }
         }
     }
+
 }
